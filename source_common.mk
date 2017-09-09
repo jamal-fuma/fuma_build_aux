eb8bf48f5dff04d3533269ae5c038146b19c471d
Jamal Natour <Jamal.Natour@fumasoftware.com>
2017-09-10 00:26:29 +0100

    Patch: provide a source_common including all modules

---
+++ source_common.mk
@@ -0,0 +1,6 @@
+include $(top_srcdir)/build-aux/include_dirs.mk
+include $(top_srcdir)/build-aux/source_defines.mk
+include $(top_srcdir)/build-aux/linker_flags.mk
+include $(top_srcdir)/build-aux/warning_flags.mk
+include $(top_srcdir)/build-aux/compile_flags.mk
+include $(top_srcdir)/build-aux/linker_libs.mk
