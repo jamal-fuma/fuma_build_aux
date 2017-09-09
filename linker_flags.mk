2f5f1869f2c4d6bb637e168f4f062ed7a66c173c
Jamal Natour <Jamal.Natour@fumasoftware.com>
2017-09-10 00:13:56 +0100

    Patch: extract  LINKER_FLAGS into linker_flags.mk

---
+++ linker_flags.mk
@@ -0,0 +1,7 @@
+LINKER_FLAGS = \
+			   $(POSTGRES_LDFLAGS) \
+			   $(WEBTOOLKIT_LDFLAGS) \
+			   $(BOOST_LDFLAGS) \
+			   $(OPENSSL_LDFLAGS) \
+			   $(PTHREAD_CFLAGS) \
+			   $(COVERAGE_LDFLAGS)
